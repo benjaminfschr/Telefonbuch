1e10ae291305a5c0f7f982a53063703c858b2997
Benjamin Fischer <benfi@unitybox.de>
2020-09-15 12:35:21 +0200

    Mit kommentaren

--- Telefonbuch.java
+++ Telefonbuch.java
@@ -17,21 +17,31 @@ public class Telefonbuch
     }
 
     // Dienste
+    /** Übergebener Eintrag wird mit der im BST integrierten Methode im BST gespeichert
+    **/
     public void eintragHinzufuegen(Telefonbucheintrag eintrag)
     {
     
     }
     
+    /** Zuerst wird im BST nach dem zu löschenden Eintrag gesucht (sucheEintrag())
+      * Ist der Eintrag gefunden wird er entfernt
+    **/
     public void eintragLoeschen(Telefonbucheintrag eintrag)
     {
     
     }
     
+    /** Im BST wird nach einem Eintrag gesucht
+      * Auf jedem Level wird der Nachname überprüft und die Methoden isGreater() und isLess() angewandt
+    **/
     public Telefonbucheintrag sucheEintrag(String nachname)
     {
         return Telefonbucheintrag;
     }
     
+    /** Zuerst wird der alte Eintrag gelöscht (eintragLoeschen), anschließend der neue eingefügt (eintragHinzufuegen) 
+    **/
     public void aendereAuftrag(Telefonbucheintrag alt, Telefonbucheintrag neu)
     {
     
